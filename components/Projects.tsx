5885c1c48f17c32f44f05cb429cf2023f4507bca
AJK_intern <mz.intern@gomwd.com>
2026-01-13 21:16:39 +0500

    style(projects): change mobile grid to 2 columns

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -182,7 +182,7 @@ const Projects: React.FC = () => {
           </a>
         </div>
 
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 md:gap-8">
+        <div className="grid grid-cols-2 lg:grid-cols-3 gap-3 md:gap-8">
           {projects.map((project, index) => (
             <motion.div
               key={project.id}
